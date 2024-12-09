8a4c6a456e48a9e1f1283175281e265f40292443
Amitkumersarkar <amitekumersarkar@gmail.com>
2024-12-09 22:24:47 +0600

    added recap addmoney function

--- Recap-Payoo-Mobile-Bank/Js-File/addMoney.js
+++ Recap-Payoo-Mobile-Bank/Js-File/addMoney.js
@@ -3,7 +3,25 @@ document.getElementById('btn-add-money').addEventListener('click', function (eve
 
     const addMoney = document.getElementById('input-add-money').value;
 
+    const addMoneyNumber = parseFloat(addMoney);
+
     const pinNumber = document.getElementById('input-pin-number').value;
 
     console.log(addMoney, pinNumber);
+
+    // wrong way to verify pin
+    if (pinNumber === '1234') {
+        const balance = document.getElementById('account-balance').innerText;
+
+        const balanceNumber = parseFloat(balance);
+
+        // adding balance
+        const newBalance = balanceNumber + addMoneyNumber;
+        // set the new-balance in innerText to get value
+        document.getElementById('account-balance').innerText = newBalance;
+        console.log(balance);
+    }
+    else {
+        alert('Failed To Add Money.! Please Try Again');
+    }
 })
